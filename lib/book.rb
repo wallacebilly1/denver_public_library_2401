9c7f09dcf2b967c2f376423561f3e1407e9d603a
Billy Wallace <wallace.billy1@gmail.com>
2024-02-20 11:33:39 -0700

    Add author instance variable to initialize method in book

--- lib/book.rb
+++ lib/book.rb
@@ -1,8 +1,9 @@
 class Book
-	attr_reader :title
+	attr_reader :title, :author
 
 	def initialize(book_details)
 		@title = book_details[:title]
+		@author = book_details[:author_first_name] + " " + book_details[:author_last_name]
 	end
 
 end
