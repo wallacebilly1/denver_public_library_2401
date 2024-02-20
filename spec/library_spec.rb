bed48f1817ebd2a88dbb733051e8bc47166963d1
Billy Wallace <wallace.billy1@gmail.com>
2024-02-20 14:02:21 -0700

    Add test for starting with an empty array of books and authors in library_spec

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -15,5 +15,10 @@ RSpec.describe Library do
 		it 'can display the library name' do
 			expect(@dpl.name).to eq "Denver Public Library"
 		end
+
+		it 'starts with an empty array of books and authors by default' do
+			expect(@dpl.books).to eq []
+			expect(@dpl.authors).to eq []
+		end
 	end
 end
