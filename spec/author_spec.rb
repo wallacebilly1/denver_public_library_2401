a311f08675de084d86998d4e2fdcd966ae1af975
Billy Wallace <wallace.billy1@gmail.com>
2024-02-20 12:04:59 -0700

    Add test for returning title of book created with write method in author_spec

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -29,5 +29,12 @@ RSpec.describe Author do
 
       expect(jane_eyre).to be_an_instance_of Book
     end
+
+		it 'can return the title of the book object' do
+			charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+			jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+
+      expect(jane_eyre.title).to eq "Jane Eyre"
+    end
 	end
 end
