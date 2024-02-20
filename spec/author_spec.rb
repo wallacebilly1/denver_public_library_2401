440a5d06251f98283a8cdc62c59b8a31fc959236
Billy Wallace <wallace.billy1@gmail.com>
2024-02-20 11:42:27 -0700

    Add test for displaying author's name to author_spec

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -5,7 +5,14 @@ RSpec.describe Author do
   describe '#initialize' do
     it 'exists' do
 			charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+
       expect(charlotte_bronte).to be_an_instance_of Author
     end
+
+		it "can display the author's name" do
+			charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+
+			expect(@author.name).to eq "Charlotte Bronte"
+  	end	
 	end
 end
