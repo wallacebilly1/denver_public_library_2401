7e3bb67eab8f736afb78108cf0890146ebf6cdb7
Billy Wallace <wallace.billy1@gmail.com>
2024-02-20 14:06:38 -0700

    Add test for add_author method in library_spec

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -5,6 +5,12 @@ require './lib/library'
 RSpec.describe Library do
 	before(:each) do 
 		@dpl = Library.new("Denver Public Library")
+		@charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+		@jane_eyre = @charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+		@professor = @charlotte_bronte.write("The Professor", "1857")
+		@villette = @charlotte_bronte.write("Villette", "1853")
+		@harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
+		@mockingbird = @harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
 	end
 
 	describe '#initialize' do
@@ -19,6 +25,15 @@ RSpec.describe Library do
 		it 'starts with an empty array of books and authors by default' do
 			expect(@dpl.books).to eq []
 			expect(@dpl.authors).to eq []
+		end
+	end
+
+	describe '#add_author' do
+		it 'can add an author to the authors array' do
+			@dpl.add_author(@charlotte_bronte)
+			@dpl.add_author(@harper_lee)
+
+			expect(@dpl.books).to eq [@charlotte_bronte, @harper_lee]
 		end
 	end
 end
