6790514675a5ece10a3b9457cece36a7e75be4ac
Billy Wallace <wallace.billy1@gmail.com>
2024-02-20 12:03:48 -0700

    Update formatting in author_spec

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -2,24 +2,24 @@ require './lib/book'
 require './lib/author'
 
 RSpec.describe Author do
-  describe '#initialize' do
-    it 'exists' do
+	describe '#initialize' do
+		it 'exists' do
 			charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
 
-      expect(charlotte_bronte).to be_an_instance_of Author
-    end
+			expect(charlotte_bronte).to be_an_instance_of Author
+		end
 
 		it "can display the author's name" do
 			charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
 
 			expect(charlotte_bronte.name).to eq "Charlotte Bronte"
-  	end
+		end
 
 		it "can display all books from an author" do
 			charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
 
 			expect(charlotte_bronte.books).to eq []
-  	end
+		end
 	end
 
 	describe '#write' do
