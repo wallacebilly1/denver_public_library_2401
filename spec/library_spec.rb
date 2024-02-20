eaa822cf1cfb0b9f8541ab0941d392b84d86c354
Billy Wallace <wallace.billy1@gmail.com>
2024-02-20 14:00:36 -0700

    Add test for returning name in library_spec

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -3,10 +3,17 @@ require './lib/author'
 require './lib/library'
 
 RSpec.describe Library do
+	before(:each) do 
+		@dpl = Library.new("Denver Public Library")
+	end
+
 	describe '#initialize' do
 		it 'exists' do
-			dpl = Library.new("Denver Public Library")
-			expect(dpl).to be_an_instance_of Library
+			expect(@dpl).to be_an_instance_of Library
+		end
+
+		it 'can display the library name' do
+			expect(@dpl.name).to eq "Denver Public Library"
 		end
 	end
 end
