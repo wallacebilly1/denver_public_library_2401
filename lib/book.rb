133feecc5ada1fb2c0e610bdb4ae0e5ffbfc1169
Billy Wallace <wallace.billy1@gmail.com>
2024-02-20 11:36:55 -0700

    Add publication_year instance variable to initialize method in book

--- lib/book.rb
+++ lib/book.rb
@@ -1,9 +1,10 @@
 class Book
-	attr_reader :title, :author
+	attr_reader :title, :author, :publication_year
 
 	def initialize(book_details)
 		@title = book_details[:title]
 		@author = book_details[:author_first_name] + " " + book_details[:author_last_name]
+		@publication_year = book_details[:publication_date][-4..-1]
 	end
 
 end
