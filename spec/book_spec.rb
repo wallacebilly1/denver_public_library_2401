eb6e7e8f1e131ff0bb35d4fcb061a055663af507
Billy Wallace <wallace.billy1@gmail.com>
2024-02-20 11:30:09 -0700

    Add test for displaying the title in book_spec

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,12 +1,17 @@
 require './lib/book'
 
 RSpec.describe Book do
-
-  let (:book) { Book.new }
+	before(:each) do 
+		@book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+	end
 
   describe '#initialize' do
     it 'exists' do
-      expect(book).to be_an_instance_of Book
+      expect(@book).to be_an_instance_of Book
     end
-  end
+
+		it 'can display the title' do
+			expect(@book.title).to eq "To Kill a Mockingbird"
+		end
+	end
 end
