bb539ad9f0d7d5720072f8b999c4f0886b2bdd8f
Billy Wallace <wallace.billy1@gmail.com>
2024-02-20 11:45:23 -0700

    Add books to inititalize method in author

--- lib/author.rb
+++ lib/author.rb
@@ -1,8 +1,9 @@
 class Author
-	attr_reader :name
+	attr_reader :name, :books
 
 	def initialize(author_details)
 		@name = author_details[:first_name] + " " + author_details[:last_name]
+		@books = []
 	end
 
 end
